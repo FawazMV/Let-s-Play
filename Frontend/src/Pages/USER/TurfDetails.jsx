ae73e7215f8a773d5c1c486212d61bbe5f9c1ef9
Muhammed Fawaz MV <mvfawazmfz@gmail.com>
2023-03-02 21:43:20 +0530

    images width height changed

--- Frontend/src/Pages/USER/TurfDetails.jsx
+++ Frontend/src/Pages/USER/TurfDetails.jsx
@@ -19,7 +19,7 @@ const PhotoManagement = ({ photos }) => {
                     <path fillRule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z" />
                 </svg>
             </button>
-            <img className="w-full shadow-2xl rounded-lg" src={photos[currentImage]} alt={`Photo ${currentImage}`} />
+            <img className="w-full max-h-[400px] lg:min-h-[400px] lg:min-w-[400px] shadow-2xl rounded-lg" src={photos[currentImage]} alt={`Photo ${currentImage}`} />
             <button className="m-3 text-gray-500" onClick={handleNextClick}  >
                 <svg width="20" height="20" fill="currentColor" viewBox="0 0 16 16">
                     <path fillRule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z" />
