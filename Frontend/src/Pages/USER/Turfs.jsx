17f7b456726e7ec78676444afc1258747aa29805
Muhammed Fawaz MV <mvfawazmfz@gmail.com>
2023-03-03 18:00:36 +0530

    ss

--- Frontend/src/Pages/USER/Turfs.jsx
+++ Frontend/src/Pages/USER/Turfs.jsx
@@ -26,7 +26,7 @@ const Turf = () => {
                         <h2 className="text-3xl font-bold">Let's Play Together</h2>
                         <p className="font-serif text-sm text-gray-400">Select your playspots and book your playtime by a tap...</p>
                     </div>
-                    <div className="grid grid-cols-1 gap-x-4 gap-y-8 md:grid-cols-2 lg:grid-cols-4">
+                    <div className="grid grid-cols-19 gap-x-4 gap-y-8 md:grid-cols-2 lg:grid-cols-42">
                         {!turfs ? <ShimmerTurf /> : turfs?.length === 0 ? <div className="w-full h-screen fixed left-0 top-0 z-[-1] flex justify-center items-center bg-gray-800 text-2xl  font-black">🙁 No Turfs Available</div> :
                             turfs.map(turf => (
                                 <TurfCard {...turf} />
