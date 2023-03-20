226b7c4a8932b763e419e86f8ee5ac63db131330
Muhammed Fawaz MV <mvfawazmfz@gmail.com>
2023-03-20 20:08:54 +0530

    Bookings route added

--- Frontend/src/App/Routers/TurfRoutes.js
+++ Frontend/src/App/Routers/TurfRoutes.js
@@ -1,3 +1,4 @@
+import Bookings from '../../Pages/Turf_Management/Bookings/Bookings'
 import Turf_Dashboard from '../../Pages/Turf_Management/Turf_Dashboard/Turf_Dashboard'
 import TurfPorfile from '../../Pages/Turf_Management/Turf_Profile/TurfProfile'
 
@@ -6,5 +7,7 @@ const TurfRoutes = [
 
     { path: '', element: <Turf_Dashboard /> },
     { path: 'profile', element: <TurfPorfile /> },
+    { path: 'bookings', element: <Bookings /> },
+    
 ]
 export default TurfRoutes
